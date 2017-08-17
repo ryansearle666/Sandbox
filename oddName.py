a5dcb427266b18a51215a98dafd1359fea40dc7c
ryan.searle <ryan.searle@my.jcu.edu.au>
2017-08-17 10:03:43 +1000

    Add name print program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,7 @@
 """Ryan Searle"""
+
+user_name = str(input("Insert name: "))
+while len(user_name) <= 0:
+    print("Insert Valid Name")
+    user_name = str(input("Insert name: "))
+print(user_name[1: :2])
